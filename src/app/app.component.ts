90879c1d878136d05cc17c7dc51e3d59d1ddec57
gmorklla <gmorklla@gmail.com>
2019-07-09 01:09:02 -0500

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'prueba';
+  windowI = {
+    id: 'number',
+    height: 'number',
+    width: 'number',
+    name: 'string',
+    controls: [
+      {
+        id: 'number',
+        name: 'string',
+        type: 'string',
+        attributes: {
+          id: 'number',
+          cols: 'string',
+          style: 'string'
+        },
+        options: [],
+        tag: 'string'
+      }
+    ],
+    designId: 'number',
+    type: 'string'
+  };
+  validaciones = [
+    {
+      id: 'number',
+      type: 'string',
+      description: 'string',
+      isAsync: 'number'
+    }
+  ];
 }
